c3d444109c4fc88e6d311e04b4cf1fc3c31ca480
bijay91lt <bijay3990@gmail.com>
2025-10-28 09:55:43 +0545

    deleteNote fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
+import { useEffect } from "react";
 import { useNoteStore } from "./useNoteStore";
 
-function App(){
-  const { notes, addNote, deleteNote } = useNoteStore ();
+function App() {
+  const { notes, addNote, deleteNote, deleteFirstNote } = useNoteStore();
+  useEffect(() => {
+    console.log("bijay logs");
+    console.log(JSON.stringify(notes, null, 2));
+  }, [notes]);
 
   return (
     <div className="min-h-screen bg-gray-50 p-6">
       <div className="max-w-2xl mx-auto">
         <h1 className="text-2xl fonr-bold mb-4">Simple Note</h1>
 
-        <button 
+        <button
           className="mb-6 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-          onClick={() => addNote('New Note', 'Write something here...')}
+          onClick={() => addNote("New Note", "Write something here...")}
         >
           + Add Note
+        </button>
+        <button
+          className="mb-6 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+          onClick={() => deleteFirstNote()}
+        >
+          delete note
         </button>
 
         <div className="spacy-y-4">
           {notes.map((note) => (
-            <div key = {note.id} className="bg-white p-4 rounded shadow">
+            <div key={note.id} className="bg-white p-4 rounded shadow">
               <h2 className="text-xl font-semibold">{note.title}</h2>
               <p className="mt-2 text-gray-700">{note.content}</p>
               <button
--- src/useNoteStore.ts
+++ src/useNoteStore.ts
@@ -1,30 +1,57 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 type Note = {
-    id: string;
-    title: string;
-    content: string;
+  id: string;
+  title: string;
+  content: string;
 };
 
-type NoteStore = { 
-    notes: Note[];
-    addNote: (title: string, content: string) => void;
-    deleteNote: (id: string) => void;
+type NoteStore = {
+  notes: Note[];
+  addNote: (title: string, content: string) => void;
+  deleteNote: (id: string) => void;
+  deleteFirstNote: () => void;
 };
 
 export const useNoteStore = create<NoteStore>((set) => ({
-    notes: [
-        { id: '1', title: 'Welcome', content: 'This is your first note!'},
-    ],
-    addNote: (title, content) => 
-        set((state) => ({
-            notes: [
-                ...state.notes,
-                {id: Date.now().toString(), title, content},
-            ],
-        })),
-    deleteNote: (id) => 
-        set((state) => ({
-            notes: state.notes.filter((note) => note.id ! == id),
-        })),
+  notes: [
+    {
+      id: Math.random().toString(),
+      title: "Welcome",
+      content: "This is your first note!",
+    },
+  ],
+  addNote: (title, content) =>
+    set((state) => {
+      const id = Math.random().toString();
+      console.log("Creating id of ", id);
+      return {
+        notes: [
+          ...state.notes,
+          {
+            id: id,
+            title: `${title + state.notes.length.toString()}`,
+            content,
+          },
+        ],
+      };
+    }),
+  deleteNote: (id) => {
+    console.log("Deleting note ID:", id, typeof id);
+    set((state) => {
+      return {
+        notes: state.notes.filter((val) => {
+          console.log("deleting", val);
+          return val.id !== id;
+        }),
+      };
+    });
+  },
+  deleteFirstNote: () => {
+    set((state) => {
+      return {
+        notes: state.notes.slice(1),
+      };
+    });
+  },
 }));
